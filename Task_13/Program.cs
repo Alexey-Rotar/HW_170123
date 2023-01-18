0efa5c386c01a1eee50bd1328c3af2e776da0b64
alexey.rotar <ar_mail@inbox.ru>
2023-01-18 20:13:43 +0500

    added readme.md

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int a, count=0, result=0;
+Console.WriteLine("Введите число:");
+if (int.TryParse(Console.ReadLine(), out a))
+{   
+	if (a<0) 
+		a*=-1;
+	result=a;
+	while (a>0)
+    	{
+        a=a/10;
+        count++;
+    	}
+	if (count>2)
+		Console.WriteLine((result % Math.Pow(10, count-2) - result % Math.Pow(10, count-3)) / Math.Pow(10, count-3));
+	else
+		Console.WriteLine("Третьей цифры нет.");
+}
+else
+    Console.WriteLine("Введено некорректное значение.");
